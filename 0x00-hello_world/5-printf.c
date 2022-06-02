32fd824e34591c7a92b0f2b36a6f7c147a15291f
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-03 01:15:57 +0300

    Basics of C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
 
 int main(void) {
 
-       	char string1[] = "with proper grammar, but the outcome is a piece of art,";
+       	char str1[] = "with proper grammar, but the outcome is a piece of art,\n";
 
-	printf(string1);
+	printf(str1);
 
 return 0;
 }
