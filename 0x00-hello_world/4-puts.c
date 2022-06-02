2e066c833c541814923a40b2c339006576f15345
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-03 01:27:54 +0300

    Basics of C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 char string[] = "\"Programming is like building a multilingual puzzle";
-
-/*writing the string to stdout*/
-
 puts(string);
-
-return 0;
+return (0);
 }
 	
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
-
-       	char str1[] = "with proper grammar, but the outcome is a piece of art,\n";
-
-	printf(str1);
-
-return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+char str1[] = "with proper grammar, but the outcome is a piece of art,\n";
+printf(str1);
+return (0);
 }
