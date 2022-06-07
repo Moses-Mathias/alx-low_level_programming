8c2aaf00e147ba16f136ab916a8c9ec038abd68a
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-08 00:27:53 +0300

    Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,19 +2,10 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ *print_to_98 - prints all natural numbers from n to 98
+ * @n: input number to check
+ * Return: nothing.
  */
-int main(void)
-{
-print_to_98(0);
-print_to_98(98);
-print_to_98(111);
-print_to_98(81);
-print_to_98(-10);
-return (0);
-}
 void print_to_98(int n)
 {
 if (n < 98)
