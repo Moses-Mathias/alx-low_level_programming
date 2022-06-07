1cdeeed499281c7f5a0f23eb5eda795e9226532c
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-08 00:23:34 +0300

    Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,15 +1,9 @@
 #include "main.h"
-
 /**
- * main - check the code
+ * times_table - function that prints the 9 times table, starting with 0
  *
- * Return: Always 0.
+ * Return: nothing.
  */
-int main(void)
-{
-    times_table();
-    return (0);
-}
 void times_table(void)
 {
 int i , j;
