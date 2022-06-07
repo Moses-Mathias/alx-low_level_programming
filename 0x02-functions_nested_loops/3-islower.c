ebaf51e9dd0d95224d7cf6f3c959ef9c57a45001
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-07 23:47:31 +0300

    Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - function that checks for lowercase character.
+ * _islower - function that checks for lowercase character.
  *
- *Returns 1 if c is lowercase
- * Returns 0 otherwise 
+ * Returns: 1 if c is lowercase
+ * Returns: 0 otherwise 
  */
 
 int _islower(int c)
