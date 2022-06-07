01cbedb037126a3a78810a7f9c07e3ce1a27ce2d
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-08 00:16:32 +0300

    Functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,16 @@
  */
 int print_last_digit(int n)
 {
-int last_digit = (n % 10);
-return (last_digit);
+int l;
+l = n % 10;
+if (l < 0)
+{
+_putchar(-l + 48);
+return (-l);
 }
-
+else
+{
+_putchar(l + 48);
+return (l);
+}
+}
