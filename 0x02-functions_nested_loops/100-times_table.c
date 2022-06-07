acffd4a993c570bbfa9b708bf4fd7c7692e54459
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-08 00:39:56 +0300

    Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,44 +7,38 @@
 
 void print_times_table(int n)
 {
-	int row, col;
-	int mult = 0;
+	int a, b, op;
 
-	if (n <= 15 && n >= 0)
+	if (n >= 0 && n <= 15)
 	{
-		for (row = 0; row <= n; row++)
+	for (a = 0; a <= n; a++)
+	{
+	_putchar(48);
+	for (b = 1; b <= n; b++)
+	{
+		op = a * b;
+		_putchar(44);
+		_putchar(32);
+		if (op <= 9)
 		{
-			for (col = 0; col <= n; col++)
-			{
-				mult = (row * col);
-				if (mult < 10)
-				{
-					if (col != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar(mult + '0');
-				}
-				else if (mult < 100)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((mult / 10) + '0');
-					_putchar((mult % 10) + '0');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar((mult / 100) + '0');
-					_putchar(((mult % 100) / 10) + '0');
-					_putchar((mult % 10) + '0');
-				}
-				if (col != n)
-					_putchar(',');
-			}
-			_putchar('\n');
+		_putchar(32);
+		_putchar(32);
+		_putchar(op + 48);
 		}
+		else if (op <= 99)
+		{
+		_putchar(32);
+		_putchar((op / 10) + 48);
+		_putchar((op % 10) + 48);
+		}
+		else
+		{
+		_putchar(((op / 100) % 10) + 48);
+		_putchar(((op / 10) % 10) + 48);
+		_putchar((op % 10) + 48);
+		}
+	}
+	_putchar('\n');
+	}
 	}
 }
