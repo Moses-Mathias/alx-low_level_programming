c92b5b8915dca761e50d896176f25946c0933398
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-07 23:35:53 +0300

    Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,10 @@
 #include "main.h"
 
 /**
- * main - function that prints 10 times the alphabet, in lowercase,.
+ * print_alphabet_x10 - function that prints 10 times the alphabet, in lowercase,.
  *
- * Return: Always 0.
+ * Return: void
  */
-int main(void)
-{
-print_alphabet_x10();
-return (0);
-}
 void print_alphabet_x10(void)
 {
 int i;
