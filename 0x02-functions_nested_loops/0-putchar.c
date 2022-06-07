3341f76ebac6c67eef3b276b9976ba4a9d5298f8
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-07 22:17:35 +0300

    Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - A program that prints "_putchar"
- * 
+ * Return: int
  * return: 0
  */
 
@@ -10,7 +10,7 @@ int main(void)
 {
 int i;
 char p[] = "_putchar";
-for (i = 0; i < 9; i++)
+for (i = 0; i < 8; i++)
 {
 _putchar(p[i]);
 }
