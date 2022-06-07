ade158440bd49352cc2d2fc91c9b3cb90f62db71
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-07 23:46:28 +0300

    Functions, nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,19 +6,7 @@
  *Returns 1 if c is lowercase
  * Returns 0 otherwise 
  */
-int main(void)
-{
-    int r;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
-}
 int _islower(int c)
 {
 if (c > 95 && c < 123)
