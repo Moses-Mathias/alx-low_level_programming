b4ae9caa074da7d91db0af3b3f4ebe397330e3ed
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-08 00:17:51 +0300

    Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - prints every minute of the day
- * starting from 00:00 to 23:59
- *
- * Return: Always 0.
+ * jack_bauer -  function that prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59.
+ * Return: nothing.
  */
 int main(void)
 {
