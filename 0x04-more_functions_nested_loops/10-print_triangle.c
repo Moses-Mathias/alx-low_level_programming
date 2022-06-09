d12348366e574b8013614919937259ad40557e09
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-10 00:43:31 +0300

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * print_triangle - to print a triangle
- * @size; gives the size of the triangle
- * Return: print_triangle
- */
+* print_triangle - prints a triangle
+* @size: takes in a integer for size of triangle
+*/
 
 void print_triangle(int size)
 {
-int row, column, k;
-for (row = 0; row < size; row++)
-{
-for (column = size - row; column > 1; column --)
-{
-_putchar(' ');
-}
-for (k = row + column; k >= 1; k--)
-{
-_putchar('#');
-}
-}
-_putchar('\n');
+	int row, column, k;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (row = 0; row < size; row++)
+	{
+		for (column = size - row; column > 1; column--)
+			_putchar(' ');
+		for (k = row + column; k >= 1; k--)
+			_putchar('#');
+		_putchar('\n');
+	}
 }
