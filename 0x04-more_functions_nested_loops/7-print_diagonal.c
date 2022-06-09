681d7f261db69e22555d71bf4b199027583403cd
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-10 00:31:09 +0300

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,30 +1,28 @@
 #include "main.h"
 
 /**
- * print_diagonal - draws a diagonal line
- * @n: variable for an integer
- * Return: int
- */
-
+* print_diagonal - draws a diagonal line
+* @n: takes in an integer
+*/
 void print_diagonal(int n)
 {
-int i;
-int j;
-for (i = 0; i < n; i++)
-{
-for (j = 0; j <= i; j++)
-{
-if (i ==j)
-{
-_putchar('\\');
-_putchar('\n');
-}
-else
-{
-_putchar(' ');
-}
-}
-}
-if ( n <= 0)
-	_putchar('\n');
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (i == j)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	}
 }
