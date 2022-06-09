4123fa5e8090104bd60d9eade9a1f944bb01cfd6
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-09 23:54:39 +0300

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_most_numbers - prints the numbers from 0 to 9
  * Does not print 2 and 4
- * @void:Returns nothing
+ * @void: Returns nothing
  * Return: void
  */
 
@@ -12,10 +12,7 @@ void print_most_numbers(void)
 char n = 48;
 while (n <= 57)
 {
-if (n = 50 || n = 52)
-{
-n++;
-}
+if (n != 50 && n != 52)
 _putchar(n);
 }
 _putchar('\n');
