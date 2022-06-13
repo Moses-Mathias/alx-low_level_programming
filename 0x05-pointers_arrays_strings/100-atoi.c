9f2384c6738805c765f3b636131c1cfd3613a9c6
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-13 23:22:17 +0300

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,28 +9,29 @@
 
 int _atoi(char *s)
 {
-int i = 0;
-int n = 0;
-int signo = 1;
-while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
-{
-if (s[i] == '-')
-signo *= -1;
-i++;
-}
-while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
-{
-if (n >= 0)
-{
-n = n * 10 - (s[i] - '0');
-i++;
-}
-else
-{
-n = n * 10 - (s[i] - '0');
-i++;
-}
-}
-signo *= -1;
-return (n *signo);
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
