b67dfe7717e91a4cc7c5fe05d53d9fc0e464c658
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-15 23:12:51 +0300

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,14 +2,15 @@
 
 /**
  * cap_string - capitazes all words of a string
- * @n: input value
+ * @s: input value
  *
  * Return: string
  */
 
 char *cap_string(char *s)
 {
-int i = 0;
+int i;
+i = 0;
 if (s[0] >= 97 && s[0] <= 122)
 {
 s[0] = s[0] - 32;
@@ -18,16 +19,19 @@ for (i = 0; s[i] != '\0'; i++)
 {
 switch (s[i])
 {
-case 44;
-case 59;
-case 46;
-case 33;
-case 63;
-case 34;
-case 40;
-case 41;
-case 123;
-case 125;
+	case 44:
+	case 59:
+	case 46:
+	case 33:
+	case 63:
+	case 34:
+	case 40:
+	case 41:
+	case 123:
+	case 125:
+	case 32:
+	case '\n':
+	case '\t':
 if (s[i + 1] >= 97 && s[i + 1] <= 122)
 {
 s[i + 1] = s[i + 1] - 32;
