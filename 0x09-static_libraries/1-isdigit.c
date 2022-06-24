8c7541de88b5eb663556f51407116ce8967f4b1e
Moses Musinde <104870051+Moses-Mathias@users.noreply.github.com>
2022-06-24 21:23:48 +0300

    C - Static libraries

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - function that checks for a digit
+ * @c: variable to use
+ * Return: int
+ */
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string pointer
+ * @s2: second string pointer
+ *
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+int cmp;
+i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+cmp = (s1[i] - s2[i]);
+return (cmp);
+}
+i++;
+}
+return (0);
+}
